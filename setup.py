6e1686d1deecc40b37a233f46f31b0012fb0bdc7
Tahsan Rahman Fahim <80458361+trfahim@users.noreply.github.com>
2025-02-25 16:45:23 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,23 @@ def is_root():
 
 if is_root():
     print("\n[+] You are running as root\n")
+    os.system("apt update")
     os.system("apt install subprocess")
     os.system("apt install pyfiglet")
     os.system("apt install colorama")
     os.system("apt install gnome-terminal")
-    os.system("apt install hcxtools")
+    os.system("apt install random string")
 
 else:
     print("\n[+] You are not running as root\n")
+    os.system("sudo apt update")
     os.system("sudo apt install subprocess")
     os.system("sudo apt install pyfiglet")
     os.system("sudo apt install colorama")
     os.system("sudo apt install gnome-terminal")
-    os.system("sudo apt install hcxtools")
+    os.system("sudo apt install random string")
 
-
-print("\nAll package install sucessful")
-print("Happy Hacking\n")
+print("\nAll package install suceessful")
+print("Happy Hacking !!\n")
 os._exit(0)
+
